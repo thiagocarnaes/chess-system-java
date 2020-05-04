6d0c52475faa70f68f98c632bba0b0625062a2c8
Thiago Carnaes <thiago.carnaes@gmail.com>
2020-05-04 10:47:54 -0300

    ChessException and ChessPosition

---
+++ src/main/java/courses/chess/ChessException.java
@@ -0,0 +1,14 @@
+package courses.chess;
+
+public class ChessException extends RuntimeException{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+    
+}
