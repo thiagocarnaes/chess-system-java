f1437d7877d9036fc01c99a923e6b94275dbc654
Thiago Carnaes <thiago.carnaes@gmail.com>
2020-05-04 10:32:42 -0300

    BoardException and defensive programing

---
+++ src/main/java/courses/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package courses.boardgame;
+
+public class BoardException extends RuntimeException{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8753099636083291872L;
+
+    public BoardException(String message) {
+        super(message);
+    }    
+    
+}
